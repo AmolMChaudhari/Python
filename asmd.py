cb28354c1afdece59c655c62011a5242b4888b20
Amol <amolmchaudhari0@gmail.com>
2023-09-27 12:28:31 +0530

    Initial Commit

---
+++ asmd.py
@@ -0,0 +1,24 @@
+#  Addition
+'''num1 = input('Enter first number: ')      
+num2 = input('Enter second number: ')    
+sum = float(num1) + float(num2)     
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum)) '''
+
+#Subtraction
+'''num1 = input('Enter first number: ')    
+num2 = input('Enter second number: ')    
+min = float(num1) - float(num2)    
+print('The subtraction of {0} and {1} is {2}'.format(num1, num2, min)) '''
+
+#Division
+'''num1 = input('Enter first number: ')      
+num2 = input('Enter second number: ')       
+div = float(num1) - float(num2)    
+print('The division of {0} and {1} is {2}'.format(num1, num2, min)) '''
+
+#Multiplication
+
+num1 = input('Enter first number: ')     
+num2 = input('Enter second number: ')       
+Mul = float(num1) * float(num2)    
+print('The multiplication of {0} and {1} is {2}'.format(num1, num2, Mul)) 
